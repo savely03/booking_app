246e0840202d56c7cbad2038408541ea59e2de88
Savely Domnikov <savely.domnikov@yandex.ru>
2023-09-12 22:24:56 +0300

    Initial commit

---
+++ src/main/java/com/github/savely03/bookingapp/BookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.savely03.bookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/savely03/bookingapp/BookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.savely03.bookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
